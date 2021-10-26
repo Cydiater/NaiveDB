2567136e6ad73bad842d925de0fdaaa6f50e342c
Cydiater <cydiater@gmail.com>
2021-10-26 15:25:16 +0800

    add sqlparser dep

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use sqlparser::dialect::GenericDialect;
+use sqlparser::parser::Parser;
+
+fn test_sql_parser() {
+    let dialect = GenericDialect {};
+    let sql = "SELECT a, b, 123, myfunc(b) \
+           FROM table_1 \
+           WHERE a > b AND b < 100 \
+           ORDER BY a DESC, b";
+    let ast = Parser::parse_sql(&dialect, sql).unwrap();
+    println!("AST: {:?}", ast);
+}
+
 fn main() {
-    println!("Hello, world!");
+    test_sql_parser();
 }
