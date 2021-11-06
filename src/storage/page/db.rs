b01ff70fbdebd68d6e65b25282a79279b23203aa
Cydiater <cydiater@gmail.com>
2021-11-06 18:58:57 +0800

    impl iter and find

--- src/storage/page/db.rs
+++ src/storage/page/db.rs
@@ -1,4 +1,5 @@
 use crate::storage::{PageID, PAGE_SIZE};
+use std::convert::TryInto;
 use thiserror::Error;
 
 /// DatabasePageBuffer is the root page of the whole DBMS storage.
@@ -51,13 +52,51 @@ impl DatabasePageBuffer {
     pub fn as_mut(&mut self) -> &mut [u8; PAGE_SIZE] {
         &mut self.buf
     }
-    // insert an record for database
+    pub fn iter(&self) -> DatabasePageIter {
+        DatabasePageIter {
+            offset: 0,
+            buf: &self.buf,
+        }
+    }
+    /// insert an record for database
     pub fn insert(&mut self, _page_id: PageID, _db_name: String) -> Result<(), PageError> {
         todo!()
     }
-    // search the page_id for a database name
-    pub fn find(&self, _db_name: String) -> Option<PageID> {
-        todo!()
+    /// search the page_id for a database name
+    pub fn find(&self, db_name: String) -> Option<PageID> {
+        for offset in self.iter() {
+            let len = u32::from_be_bytes(self.buf[offset..offset + 4].try_into().unwrap());
+            let this_db_name =
+                String::from_utf8_lossy(&self.buf[offset + 8..offset + 8 + len as usize]);
+            if db_name == this_db_name {
+                let page_id =
+                    u32::from_be_bytes(self.buf[offset + 4..offset + 8].try_into().unwrap())
+                        as PageID;
+                return Some(page_id);
+            }
+        }
+        None
+    }
+}
+
+pub struct DatabasePageIter<'a> {
+    pub offset: usize,
+    pub buf: &'a [u8],
+}
+
+impl Iterator for DatabasePageIter<'_> {
+    /// we only care offset here
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let len = u32::from_be_bytes(self.buf[self.offset..self.offset + 4].try_into().unwrap());
+        match len {
+            0 => None,
+            len => {
+                self.offset += 4 + 4 + len as usize;
+                Some(self.offset)
+            }
+        }
     }
 }
 
