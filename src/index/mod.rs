b6fce655e1083e0dc67d34df16d6a56526518295
Cydiater <cydiater@gmail.com>
2021-11-28 23:02:21 +0800

    internel node and leaf node

--- src/index/mod.rs
+++ src/index/mod.rs
@@ -1,5 +1,30 @@
-#[allow(dead_code)]
-pub struct BPTIndex {}
+use crate::storage::PageID;
+use crate::table::Datum;
+use thiserror::Error;
 
-mod key;
+#[allow(dead_code)]
+pub type RecordID = (PageID, usize);
+
+///
+/// | num_key_data_types | type[0] | type[1] | ... | page_id_of_root_node | max_child
+///
+
+#[allow(dead_code)]
+pub struct BPTIndex {
+    page_id: PageID,
+}
+
 mod node;
+
+#[allow(dead_code)]
+impl BPTIndex {
+    /// 1. fetch the root node;
+    /// 2. find the leaf node corresponding to the inserting key;
+    /// 3. have enough space ? insert => done : split => 3
+    pub fn insert(_key: Vec<Datum>, _rid: RecordID) -> Result<(), IndexError> {
+        todo!()
+    }
+}
+
+#[derive(Error, Debug)]
+pub enum IndexError {}
--- src/index/node.rs
+++ src/index/node.rs
@@ -1,5 +1,39 @@
-#[allow(dead_code)]
-pub struct InternalNode {}
+use crate::storage::{BufferPoolManagerRef, PageID};
+use crate::table::{DataType, Datum};
+use std::convert::TryInto;
+
+///
+/// | num_child | page_id[0] | key_binary | page_id[1] | ... | page_id[n] |
+///
 
 #[allow(dead_code)]
-pub struct LeafNode {}
+pub struct InternalNode {
+    page_id: PageID,
+    bpm: BufferPoolManagerRef,
+    key_data_types: Vec<DataType>,
+}
+
+#[allow(dead_code)]
+impl InternalNode {
+    pub fn key_at(_idx: usize) {}
+
+    pub fn find_child_page_id(&self, _key: Vec<Datum>) -> PageID {
+        let page = self.bpm.borrow_mut().fetch(self.page_id).unwrap();
+        let _num_child = u32::from_le_bytes(page.borrow().buffer[0..4].try_into().unwrap());
+        let _index_key_size: usize = self
+            .key_data_types
+            .iter()
+            .map(|d| d.size_as_index_key())
+            .sum();
+        todo!()
+    }
+}
+
+#[allow(dead_code)]
+pub struct LeafNode {
+    page_id: PageID,
+    bpm: BufferPoolManagerRef,
+    key_data_types: Vec<DataType>,
+}
+
+impl LeafNode {}
